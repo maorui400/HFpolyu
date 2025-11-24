9a915cd52d6a02e70c8751fb49dcb31d1156b7fc
kilig <1070036402@qq.com>
2025-11-24 13:28:48 +0800

    恢復默認打包路徑

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  // base: './', // 使用相对路径
-  base: "/polyU_hefei", // 必须与部署目录完全一致
+  base: '/', // 使用相对路径
+  // base: "/polyU_hefei", // 必须与部署目录完全一致
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
